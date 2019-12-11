cb4d647a2a478b50625ad2476324660c3588a73d
Bunborn <bdstevens@crimson.ua.edu>
2019-12-11 15:30:57 -0600

    Adds modes for flight stats and drone location

--- VehicleConnectionTest.py
+++ VehicleConnectionTest.py
@@ -8,7 +8,7 @@
 # Import DroneKit-Python
 from dronekit import connect, Command, LocationGlobal
 from pymavlink import mavutil
-import time, sys, argparse, math
+import time, sys, argparse, math, mpu
 
 # Connect to the Vehicle
 print ("Connecting")
@@ -23,10 +23,29 @@ print (" GPS: %s" % vehicle.gps_0)
 print (" Alt: %s" % vehicle.location.global_relative_frame.alt)
 print (" Heading: %s" % vehicle.heading)
 print (" Mode: %s" % vehicle.mode.name)
+print (" Connected!\nChoose operating mode:")
+mode = input("( 1 ) for general heading, ( 2 ) for distance to fountain, ( 3 ) for collision warning ")
 
-count = 0
-while count < 50:
-    print(" Heading: %s" % vehicle.heading)
-    print(" Altitude: %s" % vehicle.location.global_relative_frame.alt)
-    time.sleep(0.5)
-    count = count + 1
+if mode == 1:
+    count = 0
+    while count < 100:
+        print(" Heading: %s" % vehicle.heading)
+        print(" %s" % vehicle.location.global_frame)
+        print(" Velocity: %s" % vehicle.velocity)
+        print(" GPS: %s\n" % vehicle.gps_0)
+        time.sleep(0.1)
+        count = count + 1
+
+if mode == 2:
+    # Drone location
+    lat1 = vehicle.location.global_relative_frame.lat
+    lon1 = vehicle.location.global_relative_frame.lon
+
+    # Engineering Shelby quad fountain center
+    lat2 = 33.214837
+    lon2 = -87.542813
+
+    # Distance calculation
+    dist = mpu.haversine_distance((lat1, lon1), (lat2, lon2)) #km
+    dist = dist * 3280.24 #km -> feet
+    print("Distance to Shelby Engineering Quad Fountain : ", dist)
